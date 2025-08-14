a25a3815a577755bbbbd5053bb7a22e74322b84b
KevinMT5 <kevinalexis809@gmail.com>
2025-08-13 19:09:11 -0600

    Nuevos componentes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EmpleadosComponent } from './Empleados/empleados';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [EmpleadosComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -15,5 +16,5 @@ export class App {
 
 
 
-  
+
 }
